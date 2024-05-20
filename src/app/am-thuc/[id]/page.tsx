d2b411fe0d3db66c475c24e54484d5de5dba1fba
daothehung <daohung447@gmail.com>
2024-05-20 11:29:14 +0700

    test:test

--- src/app/am-thuc/[id]/page.tsx
+++ src/app/am-thuc/[id]/page.tsx
@@ -1,20 +1,23 @@
 import { notFound, redirect } from 'next/navigation';
-import { getPost } from '@/lib/lib';
+import { getPost, getPosts } from '@/lib/lib';
 import { H1Block, H2Block, ImageBlock, PBlock } from '@/components/post-block';
 import Markdown from 'markdown-to-jsx';
+import { TPost } from '@/types';
 
+type Tdata = {
+	params: {
+		id: string
+	}
+}
+export default async function Page({ params }: Tdata) {
 
-export default async function Page({
-	params: { id },
-}: {
-	params: { id: string; url: string };
-}) {
-	const post = await getPost(id, 'am-thuc');
-	if (!post) return redirect(`/am-thuc`);
+	// const post = await getPost(params.id, 'am-thuc')
+	// if (!post) return redirect(`/am-thuc`);
 	return (
 		<div className="px-3 lg:px-0 w-full md:w-[750px] mx-auto  lg:w-[794px]">
 			<div className="flex justify-center items-center flex-col">
-				<div className="flex justify-center items-center flex-col gap-4 w-full">
+				{params.id}
+				{/* <div className="flex justify-center items-center flex-col gap-4 w-full">
 					<div className="text-xl lg:text-4xl font-bold">
 						{post.title}
 					</div>
@@ -39,8 +42,24 @@ export default async function Page({
 					<div className="w-full text-end font-bold  lg:text-xl">
 						{post.author}
 					</div>
-				</div>
+				</div> */}
 			</div>
 		</div>
 	);
+}
+
+
+
+// // or Dynamic metadata
+// export async function generateStaticParams() {
+// 	const posts = await getPosts('am-thuc');
+
+// 	return posts.map((post) => ({ slug: post.title }))
+
+// }
+export async function generateStaticParams() {
+	const posts = await getPosts('am-thuc');
+	return posts.map((post) => ({
+		slug: post.title,
+	}))
 }
