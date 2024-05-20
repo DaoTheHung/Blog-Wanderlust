2d7e42937f374b50992d34c790524e085a01eb97
daothehung <daohung447@gmail.com>
2024-05-20 11:05:03 +0700

    feat: refactor lib

--- src/app/am-thuc/[id]/page.tsx
+++ src/app/am-thuc/[id]/page.tsx
@@ -1,8 +1,7 @@
 import { notFound, redirect } from 'next/navigation';
-import { getPost, getPosts } from '@/lib/lib';
+import { getPost } from '@/lib/lib';
 import { H1Block, H2Block, ImageBlock, PBlock } from '@/components/post-block';
 import Markdown from 'markdown-to-jsx';
-import { convertToSlug } from '@/features/untils';
 
 
 export default async function Page({
@@ -10,20 +9,20 @@ export default async function Page({
 }: {
 	params: { id: string; url: string };
 }) {
-	const posts = await getPosts('am-thuc');
-	const post = posts.find((post) => convertToSlug(post.title) === id)
+	const post = await getPost(id, 'am-thuc');
+	if (!post) return redirect(`/am-thuc`);
 	return (
 		<div className="px-3 lg:px-0 w-full md:w-[750px] mx-auto  lg:w-[794px]">
 			<div className="flex justify-center items-center flex-col">
 				<div className="flex justify-center items-center flex-col gap-4 w-full">
 					<div className="text-xl lg:text-4xl font-bold">
-						{post?.title}
+						{post.title}
 					</div>
 					<div className="text-xl font-bold">
-						{post?.description}
+						{post.description}
 					</div>
 					<div className="">
-						{post?.body && <Markdown
+						<Markdown
 							options={{
 								overrides: {
 									h1: H1Block,
@@ -33,12 +32,12 @@ export default async function Page({
 								}
 							}}
 						>
-							{post?.body}
-						</Markdown>}
+							{post.body}
+						</Markdown>
 					</div>
 
 					<div className="w-full text-end font-bold  lg:text-xl">
-						{post?.author}
+						{post.author}
 					</div>
 				</div>
 			</div>
--- src/lib/lib.ts
+++ src/lib/lib.ts
@@ -1,23 +1,29 @@
 import { convertToSlug } from '@/features/untils';
 import { TPost } from '@/types';
-import fs from 'fs/promises';
+import fs from 'fs';
 import matter from 'gray-matter';
 import path from 'path';
 
 export async function getPosts(url: string) {
-	const posts = await fs.readdir(`./posts/${url}`);
-
-	return Promise.all(
-		posts
-			.filter((file) => path.extname(file) === '.mdx')
-			.map(async (file) => {
-				const filePath = `./posts/${url}/${file}`;
-				const fileContent = await fs.readFile(filePath, 'utf8');
-				const { data, content } = matter(fileContent);
-
-				return { ...data, body: content } as TPost;
-			})
-	);
+	const folder = 'posts/' + url + '/'
+	const files = fs.readdirSync(folder)
+	const markdownPosts = files.filter(file => file.endsWith('.mdx'))
+	// get the file data
+	const posts = markdownPosts.map((filename) => {
+		const fileContents = fs.readFileSync(`${folder}${filename}`, 'utf8')
+		const matterResult = matter(fileContents)
+		return {
+			title: matterResult.data.title,
+			id: filename.replace('.mdx', ''),
+			description: matterResult.data.description,
+			date: matterResult.data.date,
+			author: matterResult.data.author,
+			image: matterResult.data.image,
+			slug: url,
+			body: matterResult.content,
+		}
+	})
+	return posts
 }
 
 export async function getPost(slug: string, url: string) {
