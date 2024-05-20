ac4a5f8d224e14b9bf9a442c5a9d60d2d64468bc
daothehung <daohung447@gmail.com>
2024-05-20 10:50:41 +0700

    update

--- src/app/am-thuc/[id]/page.tsx
+++ src/app/am-thuc/[id]/page.tsx
@@ -1,27 +1,29 @@
 import { notFound, redirect } from 'next/navigation';
-import { getPost } from '@/lib/lib';
+import { getPost, getPosts } from '@/lib/lib';
 import { H1Block, H2Block, ImageBlock, PBlock } from '@/components/post-block';
 import Markdown from 'markdown-to-jsx';
+import { convertToSlug } from '@/features/untils';
+
 
 export default async function Page({
 	params: { id },
 }: {
 	params: { id: string; url: string };
 }) {
-	const post = await getPost(id, 'am-thuc');
-	if (!post) return redirect(`/am-thuc`);
+	const posts = await getPosts('am-thuc');
+	const post = posts.find((post) => convertToSlug(post.title) === id)
 	return (
 		<div className="px-3 lg:px-0 w-full md:w-[750px] mx-auto  lg:w-[794px]">
 			<div className="flex justify-center items-center flex-col">
 				<div className="flex justify-center items-center flex-col gap-4 w-full">
 					<div className="text-xl lg:text-4xl font-bold">
-						{post.title}
+						{post?.title}
 					</div>
 					<div className="text-xl font-bold">
-						{post.description}
+						{post?.description}
 					</div>
 					<div className="">
-						<Markdown
+						{post?.body && <Markdown
 							options={{
 								overrides: {
 									h1: H1Block,
@@ -31,12 +33,12 @@ export default async function Page({
 								}
 							}}
 						>
-							{post.body}
-						</Markdown>
+							{post?.body}
+						</Markdown>}
 					</div>
 
 					<div className="w-full text-end font-bold  lg:text-xl">
-						{post.author}
+						{post?.author}
 					</div>
 				</div>
 			</div>
