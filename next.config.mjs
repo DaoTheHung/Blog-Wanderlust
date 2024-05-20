e2b76e32bd46e11cf4a57cad3c50e389a8bf5ff1
DaoTheHung <Daohung447@gmail.com>
2024-05-20 19:21:36 +0700

    feat: updater

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,63 @@ import { build } from 'velite'
 
 /** @type {import('next').NextConfig} */
 export default {
+	images: {
+		domains: [
+			'hnm.1cdn.vn',
+			'i1-dulich.vnecdn.net',
+			'i1-ngoisao.vnecdn.net',
+			'bvhttdl.mediacdn.vn',
+			'static.wixstatic.com',
+			'picsum.photos',
+			'res.cloudinary.com',
+			'imagedelivery.net',
+			'customer-8p6222sl725afjta.cloudflarestream.com',
+			'cdn.tuoitre.vn',
+			'www.vietnam.vn',
+			'hnm.1cdn.vn',
+			'i1-dulich.vnecdn.net',
+			'i1-ngoisao.vnecdn.net',
+			'bvhttdl.mediacdn.vn',
+			'cdn3.ivivu.com',
+			'comly.vn',
+			'toquoc.mediacdn.vn',
+			'cafefcdn.com',
+		],
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'wanderlusttips.asia',
+			},
+			{
+				protocol: 'https',
+				hostname: 'vntravel.org.vn',
+			},
+			{
+				protocol: 'https',
+				hostname: 'www.vtr.org.vn',
+			},
+			{
+				protocol: 'https',
+				hostname: 'cdn.tuoitre.vn',
+			},
+			{
+				protocol: 'https',
+				hostname: 'media.vov.vn',
+			},
+			{
+				protocol: 'https',
+				hostname: 'cdn3.ivivu.com',
+			},
+			{
+				protocol: 'https',
+				hostname: 'comly.vn',
+			},
+			{
+				protocol: 'https',
+				hostname: 'toquoc.mediacdn.vn',
+			},
+		],
+	},
   // othor next config here...
   webpack: config => {
     config.plugins.push(new VeliteWebpackPlugin())
